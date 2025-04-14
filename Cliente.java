08092ee43465039f756ca4380ddd00f8a48c72d6
Jotape <jp25092006@gmail.com>
2025-04-13 23:54:04 -0300

    getters setters Clientes

---
+++ Cliente.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(String nome, String cpf, String telefone, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // Setters, se quiser editar depois
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
