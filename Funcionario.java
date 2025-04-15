91d4d637289207f8bc64ee3f8b59922849ce4d58
Jotape <jp25092006@gmail.com>
2025-04-15 17:31:33 -0300

    Adição da Class Funcionario

---
+++ Funcionario.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+
+    public Funcionario(String nome, String cpf, String telefone, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    public String getEndereco() {
+        return endereco;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }   
+    
+    public void exibirFuncionario() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Endereco: " + endereco);
+        System.out.println("------------------------");
+    }
+    
+    public void adicionarFuncionario() {
+        Scanner sc = new Scanner(System.in);
+        Funcionario novoFuncionario = new Funcionario(nome, cpf, telefone, endereco);
+        
+        System.out.println("Cadastro de Funcionario");
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
+        System.out.print("Telefone: ");
+        String telefone = sc.nextLine();    
+        System.out.println("Endereco: ");
+        String endereco = sc.nextLine();
+        
+        novoFuncionario.setNome(nome);
+        novoFuncionario.setCpf(cpf);
+        novoFuncionario.setTelefone(telefone);
+        novoFuncionario.setEndereco(endereco);
+        
+        System.out.println("Funcionario cadastrado com sucesso!");
+    }
+    
+    public void listarFuncionarios() {
+        System.out.println("\n--- Lista de Funcionarios ---");
+        exibirFuncionario();
+    }
+}
