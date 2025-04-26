78f633023d5063c441c61cad90a0c5caeae06b7a
Lucas <lucas@tinaglini.com>
2025-04-26 16:53:56 -0300

    Main

--- src/main/java/com/audiomania/estoque/Main.java
+++
@@ -1,8 +0,0 @@
-package com.audiomania.estoque;
-
-public class Main {
-    public static void main(String[] args) {
-        ProdutoRepository gerenciador = new ProdutoRepository();
-        gerenciador.exibirMenu();
-    }
-}
